0a1278d87c6db2f9781a2486c5382b3e04014e92
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-02-13 10:43:02 -0800

    add all passing specs, ready for web functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 var triangleType = function( side1, side2, side3 ) {
 
   if (( side1 + side2 <= side3) || ( side1 + side3 <= side2 ) || ( side2 + side3 <= side1 )) {
-    return true;
-  } else {
-    return false;
+    return("Not a triangle");
   }
-
+    else if ( side1 === side2 && side2 === side3 ) {
+      return("Equilateral");
+    }
+    else if ( side1 !== side2 && side2 !== side3 && side3 !== side1 ) {
+      return("Scalene");
+    }
+    else if (( side1 === side2 && side2 !== side3) || (side2 === side3 && side3 !== side1) || (side1 === side3 && side3 !== side2)) {
+      return("Isosceles");
+    }
 };
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,16 @@
 describe("triangleType", function() {
   it("confirms if the user input forms a triangle", function() {
-    expect(triangleType([2, 2, 8])).to.eql(false);
+    expect(triangleType(2, 2, 8)).to.eql("Not a triangle");
   });
 
-  it("tells the user what type of triangle they have", function() {
-    expect(triangleType([3, 3, 3])).to.eql("Equilateral");
+  it("tells the user if they have an equilateral triangle", function() {
+    expect(triangleType(3, 3, 3)).to.eql("Equilateral");
+  });
+
+  it("tells the user if they have a Scalene triangle", function() {
+    expect(triangleType(2, 3, 4)).to.eql("Scalene");
+  });
+  it("tells the user if they have an Isosceles triangle", function() {
+    expect(triangleType(2, 2, 3)).to.eql("Isosceles");
   });
 });
