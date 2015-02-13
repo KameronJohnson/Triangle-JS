44e7f256e8ef4b50ed9782ae3e4225e319e1141c
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-02-13 10:09:34 -0800

    add first passing spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-var functionName = function( userInput ) {
+var triangleType = function( side1, side2, side3 ) {
 
-// ???
+  if (( side1 + side2 <= side3) || ( side1 + side3 <= side2 ) || ( side2 + side3 <= side1 )) {
+    return true;
+  } else {
+    return false;
+  }
 
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,9 @@
+describe("triangleType", function() {
+  it("confirms if the user input forms a triangle", function() {
+    expect(triangleType([2, 2, 8])).to.eql(false);
+  });
 
+  it("tells the user what type of triangle they have", function() {
+    expect(triangleType([3, 3, 3])).to.eql("Equilateral");
+  });
+});
